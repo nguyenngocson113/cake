bd92c9f139ead3f4e3a569f94c962ae1e8cef696
ngocson <ngocson11394@gmail.com>
2016-11-05 08:59:17 +0700

    fix postgres

--- model/database.js
+++ model/database.js
@@ -1,6 +1,6 @@
 // config/database.js
 module.exports = {
 
-url: "postgres://postgres:1@localhost:5432/cookie",
+url: "postgres://aunlyclvmnejuf:AoqzgaKWJRUCw5e_t6abW0fRgu@ec2-184-73-196-82.compute-1.amazonaws.com:5432/d566cu8nnq95ph",
 
 };
