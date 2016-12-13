5fc89e631f643da795ec29f4b00f9e8e47d93203
ngocson <ngocson11394@gmail.com>
2016-12-14 00:37:46 +0700

    13-12

--- model/database.js
+++ model/database.js
@@ -1,6 +1,6 @@
 // config/database.js
 module.exports = {
 
-url: "postgres://aunlyclvmnejuf:AoqzgaKWJRUCw5e_t6abW0fRgu@ec2-184-73-196-82.compute-1.amazonaws.com:5432/d566cu8nnq95ph",
+url: "postgres://postgres:1@ec2-184-73-196-82.compute-1.amazonaws.com:8080/cookie",
 
 };
